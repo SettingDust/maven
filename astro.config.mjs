a90be3136695c061e1748dcab72e2f0f8f8b3f20
SettingDust <settingdust@gmail.com>
2025-12-25 17:27:16 +0800

    feat: add custom site name and fix repository URLs
    - Support custom siteName in config
    - Fix base path in repository URLs
    - Add name to Gradle configs
    - Update CI to use pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://settingdust.github.io/maven',
-  base: '/',
+  site: 'https://settingdust.github.io/',
+  base: '/maven/',
   build: {
     assets: '_astro'
-  }
+  },
+  siteName: 'SettingDust\'s Maven'
 });
